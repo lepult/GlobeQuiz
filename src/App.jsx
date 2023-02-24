cf9a458fb77562a9e5c846fd7901ba00002d68ea
LePult <leopoldseins@gmail.com>
2023-02-24 23:55:27 +0100

    :art: Create Round component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import Map from './map/round/Map';
-import MiniMap from './map/round/Minimap';
 import StartGame from './map/start-game/StartGame';
 import RoundFinished from './map/round-finished/RoundFinished';
-import Score from './map/Score';
 import GameFinished from './map/game-finished/GameFinished';
 import { useSelector } from 'react-redux';
 import { selectGameFinished, selectRoundFinished, selectRoundNumber } from './redux-modules/game/gameSelector';
-
-
-
+import Round from './map/round/Round';
 
 const App = () => {
   const gameFinished = useSelector(selectGameFinished);
@@ -23,19 +17,7 @@ const App = () => {
   
   if (roundFinished) return <RoundFinished/>;
 
-  return (
-    <div
-      style={{
-        width: '100vw',
-        height: '100vh',
-        position: 'relative',
-      }}
-    >
-      <Score/>
-      <Map/>
-      <MiniMap/>
-    </div>
-  )
+  return <Round/>;
 }
 
 export default App;
