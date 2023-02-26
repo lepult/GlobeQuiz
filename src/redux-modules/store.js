fd063ed7c595bc6d3effce0112c458b294a74138
LePult <leopoldseins@gmail.com>
2023-02-26 13:24:59 +0100

    Create build

--- src/redux-modules/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import gameReducer from './game/gameSlice';
-
-export const store = configureStore({
-    reducer: {
-        game: gameReducer,
-    },
-})
--- src/utils/mapHelper.js
+++
@@ -1,9 +0,0 @@
-export const restrictViewBounds = ({viewState, oldViewState}) => {
-    if (Math.abs(viewState.longitude - oldViewState.longitude) > 180) {
-        viewState.longitude = Math.sign(oldViewState.longitude) * 180;
-    }
-    return viewState;
-}
-
-
-  
